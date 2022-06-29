e67e154d7c17cafce536e35d29f3485b03c701e4
HolyOly <olvstep@mail.ru>
2022-06-29 23:45:54 +0300

    feat: add script for start button

---
+++ script/startApp.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let btnStart = document.getElementById('btn-start'); 
+let ball = document.getElementById('img-ball');
+
+function startApp() {
+  btnStart.classList.add('display-none');
+  ball.classList.add('display-none');
+}
+
+btnStart.addEventListener('click', startApp);
